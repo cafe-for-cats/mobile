3672242b14fddf84523540299da1e8cecc84fd3f
dgatto <dgattoiii@gmail.com>
2020-12-30 14:49:49 -0500

    Fix tests, maybe.

--- api/src/tests/pins.spec.ts
+++ api/src/tests/pins.spec.ts
@@ -1,30 +1,27 @@
 import server from '../server';
 import supertest from 'supertest';
 import { expect } from 'chai';
-import { doesNotMatch } from 'assert';
 
 describe('route pins/', () => {
-  it('should GET all pins', async done => {
-    await supertest(server)
+  it('should GET all pins', async () => {
+    return await supertest(server)
       .get('/pins')
       .expect(200)
       .then(res => {
         expect(res.body.length).to.be.greaterThan(1);
-        done();
       });
   });
 
-  it('should GET one pin', async done => {
-    await supertest(server)
+  it('should GET one pin', async () => {
+    return await supertest(server)
       .get('/pins/5fe6d05a07e2e1401b003106')
       .expect(200)
       .then(res => {
         expect(res.body._id).to.exist;
-        done();
       });
   });
 
-  it('should POST a pin with all fields', async done => {
+  it('should POST a pin with all fields', async () => {
     const data = {
       label: 'automation_test',
       userId: 999999,
@@ -34,24 +31,22 @@ describe('route pins/', () => {
       lng: -9.01
     };
 
-    await supertest(server)
+    return await supertest(server)
       .post('/pins')
       .send(data)
       .expect(200);
-    done();
   });
 
-  it('should not POST a pin with no fields', async done => {
+  it('should not POST a pin with no fields', async () => {
     const data = {};
 
-    await supertest(server)
+    return await supertest(server)
       .post('/pins')
       .send(data)
       .expect(422);
-    done();
   });
 
-  it('should PATCH a pin', async done => {
+  it('should PATCH a pin', async () => {
     const data = {
       label: 'automation_test',
       userId: 999999,
@@ -63,32 +58,30 @@ describe('route pins/', () => {
 
     const id = '5fe6d05a07e2e1401b003106';
 
-    await supertest(server)
+    return await supertest(server)
       .patch(`/pins/${id}`)
       .send(data)
       .expect(200)
       .then(res => {
         expect(res.body).to.exist;
-        done();
       });
   });
 
-  it('should not PATCH a pin with no fields', async done => {
+  it('should not PATCH a pin with no fields', async () => {
     const data = {};
 
     const id = '5fe6d05a07e2e1401b003106';
 
-    await supertest(server)
+    return await supertest(server)
       .patch(`/pins/${id}`)
       .send(data)
       .expect(200)
       .then(res => {
         expect(res.body).to.exist;
-        done();
       });
   });
 
-  it('should DELETE a pin', async done => {
+  it('should DELETE a pin', async () => {
     const data = {
       label: 'automation_test',
       userId: 999999,
@@ -96,14 +89,13 @@ describe('route pins/', () => {
       lng: -9.01
     };
 
-    await supertest(server)
+    return await supertest(server)
       .post('/pins')
       .send(data)
       .then(async res => {
-        await supertest(server)
+        return await supertest(server)
           .delete(`/pins/${res.body._id}`)
           .expect(200);
-        done();
       });
   });
 });
