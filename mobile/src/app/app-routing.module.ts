ca8fae4e9e2595f3d15d22006d75cf73be4dc5c6
dgatto <dgattoiii@gmail.com>
2021-01-07 14:59:56 -0500

    Adds routing for tabs between home and settings.

--- mobile/src/app/app-routing.module.ts
+++ mobile/src/app/app-routing.module.ts
@@ -3,19 +3,9 @@ import { PreloadAllModules, RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   {
-    path: 'home',
-    loadChildren: () =>
-      import('./pages/home/home.module').then(m => m.HomePageModule)
-  },
-  {
     path: '',
-    redirectTo: 'home',
-    pathMatch: 'full'
-  },
-  {
-    path: 'settings',
     loadChildren: () =>
-      import('./pages/settings/settings.module').then(m => m.SettingsPageModule)
+      import('./pages/tabs/tabs.module').then(m => m.TabsPageModule)
   }
 ];
 
