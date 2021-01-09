073ef4fff5e81f2f61843bd3ecd7ece17f3189c5
dgatto <dgattoiii@gmail.com>
2021-01-08 19:19:02 -0500

    Can now set local settings

--- mobile/src/app/pages/settings/settings.module.ts
+++ mobile/src/app/pages/settings/settings.module.ts
@@ -3,18 +3,11 @@ import { CommonModule } from '@angular/common';
 import { FormsModule } from '@angular/forms';
 
 import { IonicModule } from '@ionic/angular';
-
 import { SettingsPageRoutingModule } from './settings-routing.module';
-
 import { SettingsPage } from './settings.page';
 
 @NgModule({
-  imports: [
-    CommonModule,
-    FormsModule,
-    IonicModule,
-    SettingsPageRoutingModule
-  ],
+  imports: [CommonModule, FormsModule, IonicModule, SettingsPageRoutingModule],
   declarations: [SettingsPage]
 })
 export class SettingsPageModule {}
--- mobile/src/app/pages/settings/settings.page.ts
+++ mobile/src/app/pages/settings/settings.page.ts
@@ -1,15 +1,55 @@
 import { Component, OnInit } from '@angular/core';
+import { Storage } from '@ionic/storage';
 
 @Component({
   selector: 'app-settings',
   templateUrl: './settings.page.html',
-  styleUrls: ['./settings.page.scss'],
+  styleUrls: ['./settings.page.scss']
 })
-export class SettingsPage implements OnInit {
+export class SettingsPage {
+  changes: { settingName: string; value: string }[] = [];
 
-  constructor() { }
+  constructor(private storage: Storage) {}
 
-  ngOnInit() {
+  radioGroupChange(settingName, event) {
+    const settingWithGivenName = this.changes.find(
+      x => x.settingName == settingName
+    );
+
+    if (settingWithGivenName) {
+      settingWithGivenName.value = event.detail.value;
+    } else {
+      this.changes.push({ settingName, value: event.detail.value });
+    }
   }
 
+  async handleClick() {
+    this.remove(this.changes[0].settingName);
+
+    this.changes &&
+      this.changes.forEach(async change => {
+        await this.set(change.settingName, change.value);
+        const name = await this.get(change.settingName);
+        console.log(name);
+      });
+
+    this.changes = [];
+  }
+
+  async set(settingName, value) {
+    return await this.storage.set(`setting:${settingName}`, value);
+  }
+
+  async get(settingName) {
+    return await this.storage.get(`setting:${settingName}`);
+  }
+
+  async remove(settingName) {
+    return await this.storage.remove(`setting:${settingName}`);
+  }
+
+  async clear() {
+    await this.storage.clear();
+    return;
+  }
 }
