96dddcf6cbf34a0be331da9a7ecb8d651ef0ba21
dgatto <dgattoiii@gmail.com>
2021-02-19 00:27:52 -0500

    Adds pino to packagejson; Wires up endpoints in socket.io framework.

--- api/src/server.ts
+++ api/src/server.ts
@@ -1,25 +1,39 @@
 import express from 'express';
 import * as socketio from 'socket.io';
 import * as path from 'path';
+import { Socket } from 'net';
+import { IPin } from './models/pinModels';
+import pino from 'pino';
 
 const app = express();
-app.set('port', process.env.PORT || 3000);
 
+const logger = pino({
+  level: process.env.LOG_LEVEL || 'trace',
+  prettyPrint: {
+    levelFirst: true,
+    translateTime: true,
+    ignore: 'pid,hostname'
+  }
+});
+
+app.set('port', process.env.PORT || 3000);
 let http = require('http').Server(app);
-// set up socket.io and bind it to our
-// http server.
 let io = require('socket.io')(http);
 
 app.get('/', (req: any, res: any) => {
   res.sendFile(path.resolve('./src/view/index.html'));
 });
 
-// whenever a user connects on port 3000 via
-// a websocket, log that a user has connected
-io.on('connection', function(socket: any) {
+io.on('connection', (socket: Socket) => {
   console.log('a user connected');
-  socket.on('message', function(message: any) {
-    console.log(message);
+
+  socket.on('getPins', () => {
+    const pins: string[] = ['one', 'two'];
+    socket.emit('getPins', JSON.stringify(pins));
+  });
+
+  socket.on('updatePin', (input: any) => {
+    socket.emit('updatePin', 'success');
   });
 });
 
