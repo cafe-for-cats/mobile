5bc8f54347bdcd8d11341507759be5318cf8b11f
dgatto <dgattoiii@gmail.com>
2021-04-06 19:22:06 -0400

    Fix tests; xdescribe google maps test.

--- mobile/src/app/components/login/login.component.spec.ts
+++ mobile/src/app/components/login/login.component.spec.ts
@@ -1,5 +1,10 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { FormBuilder, FormsModule } from '@angular/forms';
+import { Router } from '@angular/router';
 import { IonicModule } from '@ionic/angular';
+import { of } from 'rxjs';
+import { JwtService } from 'src/app/services/jwt.service';
 
 import { LoginComponent } from './login.component';
 
@@ -9,8 +14,25 @@ describe('LoginComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ LoginComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [LoginComponent],
+      imports: [IonicModule.forRoot(), HttpClientTestingModule],
+      providers: [
+        {
+          provide: JwtService,
+          useValue: {
+            login: () => of({ token: 'token_string' }),
+            register: () => of({ token: 'token_string' }),
+            logout: () => {},
+          },
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate: () => {},
+          },
+        },
+        FormBuilder,
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(LoginComponent);
--- mobile/src/app/components/welcome/welcome.component.spec.ts
+++ mobile/src/app/components/welcome/welcome.component.spec.ts
@@ -1,4 +1,7 @@
+import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import { Router } from '@angular/router';
+import { JwtHelperService } from '@auth0/angular-jwt';
 import { IonicModule } from '@ionic/angular';
 
 import { WelcomeComponent } from './welcome.component';
@@ -9,8 +12,22 @@ describe('WelcomeComponent', () => {
 
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ WelcomeComponent ],
-      imports: [IonicModule.forRoot()]
+      declarations: [WelcomeComponent],
+      imports: [IonicModule.forRoot(), HttpClientTestingModule],
+      providers: [
+        {
+          provide: JwtHelperService,
+          useValue: {
+            isTokenExpired: () => true,
+          },
+        },
+        {
+          provide: Router,
+          useValue: {
+            navigate: () => {},
+          },
+        },
+      ],
     }).compileComponents();
 
     fixture = TestBed.createComponent(WelcomeComponent);
