af70135808f294c3b6af7619200ad833dc7496e6
milkndcoffee <milkndcoffee@gmail.com>
2021-05-06 16:57:52 -0700

    used prettier format and added requested comments on types/interfaces

--- mobile/src/app/components/resources/resources.component.ts
+++ mobile/src/app/components/resources/resources.component.ts
@@ -1,32 +1,57 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit } from "@angular/core";
 
 @Component({
-  selector: 'app-resources',
-  templateUrl: './resources.component.html',
-  styleUrls: ['./resources.component.scss'],
+  selector: "app-resources",
+  templateUrl: "./resources.component.html",
+  styleUrls: ["./resources.component.scss"],
 })
 export class ResourcesComponent implements OnInit {
   data: ArrayResource = [
-    {'General Information': [{ infoName: 'What to expect at a protest', url: 'https://www.law.nyu.edu/centers/race-inequality-law/protest-tips' }]},
-    {'Find Legal Resources': [{ infoName: 'NGL Legal Support Hotline', url: 'https://www.communityjusticeexchange.org/nbfn-directory' }, { infoName: 'National Bail Fund Network', url: 'https://www.nlg.org/massdefenseprogram/' }]},
-    {'Support us': [{ infoName: 'GoFundMe', url: 'https://www.gofundme.com/f/help-build-a-protest-app' }]}
-  ]
+    {
+      "General Information": [
+        {
+          infoName: "What to expect at a protest",
+          url:
+            "https://www.law.nyu.edu/centers/race-inequality-law/protest-tips",
+        },
+      ],
+    },
+    {
+      "Find Legal Resources": [
+        {
+          infoName: "NGL Legal Support Hotline",
+          url: "https://www.communityjusticeexchange.org/nbfn-directory",
+        },
+        {
+          infoName: "National Bail Fund Network",
+          url: "https://www.nlg.org/massdefenseprogram/",
+        },
+      ],
+    },
+    {
+      "Support us": [
+        {
+          infoName: "GoFundMe",
+          url: "https://www.gofundme.com/f/help-build-a-protest-app",
+        },
+      ],
+    },
+  ];
 
-  constructor() { }
+  constructor() {}
   ngOnInit() {}
-
-  //delete later if unused and utilising ionic button href
-  redirectTo(url){
-    console.log(url)
-    window.location.href=url
-  }
 }
 
-//types and interfaces
-type Resource = Record<string, Array<ResourceDesc>>
-type ArrayResource = Array<Resource>
+/** Associates a list of resources to their related resource section.  */
+type Resource = Record<string, Array<ResourceDesc>>;
+/** A list of resources */
+type ArrayResource = Array<Resource>;
 
+/** Defines an individual resource item on the page */
 interface ResourceDesc {
-  infoName: string
-  url: string
+  /** Defines an individual resource item on the page */
+  infoName: string;
+
+  /** The url redirect for the item */
+  url: string;
 }
