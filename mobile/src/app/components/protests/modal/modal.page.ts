cc43dda55aab32213116e455665a05ac39ba51dc
milkndcoffee <milkndcoffee@gmail.com>
2021-05-18 17:24:15 -0700

    removed modal

--- mobile/src/app/components/protests/modal/modal.page.ts
+++
@@ -1,9 +0,0 @@
-import { Component, Input } from '@angular/core';
-
-@Component({
-  selector: 'modal-page',
-  templateUrl: './modal.page.html',
-})
-export class ModalPage {
-  constructor() {}
-}
