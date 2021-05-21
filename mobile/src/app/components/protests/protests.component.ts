3b3ccf68d2d8b868288e6f99357b186a2e54de23
milkndcoffee <milkndcoffee@gmail.com>
2021-05-20 17:33:16 -0700

    added startDate input to Protests

--- mobile/src/app/components/protests/protests.component.ts
+++ mobile/src/app/components/protests/protests.component.ts
@@ -3,7 +3,6 @@ import { Component, OnInit } from '@angular/core';
 import { FormBuilder, Validators } from '@angular/forms';
 import { Router } from '@angular/router';
 import { ModalController } from '@ionic/angular';
-import { ModalPage } from '../modal/modal.page';
 import { Observable } from 'rxjs';
 import { map } from 'rxjs/operators';
 import { JwtService } from 'src/app/services/jwt.service';
@@ -19,12 +18,13 @@ export class ProtestsComponent implements OnInit {
   form = this.formBuilder.group({
     title: [null, Validators.required],
     description: [null, Validators.required],
+    startDate: [null, Validators.required],
   });
 
   data$: Observable<{}>;
   addResult$: Observable<{ status: boolean }>;
-
   protestAdded$: Observable<any>;
+  minDate: String;
 
   constructor(
     private formBuilder: FormBuilder,
@@ -40,16 +40,9 @@ export class ProtestsComponent implements OnInit {
   ngOnInit() {
     const userId = this.jwtService.token.user.id;
     const baseUrl = 'http://localhost:3000/protests/getProtestsView';
+    this.minDate = this.setMinDate();
 
-    this.presentToast('ngOnInit.');
     this.dataService.requestGetProtestsForUser(); //emits the websocket -> grabs data
-  }
-
-  async presentModal() {
-    const modal = await this.modalController.create({
-      component: ModalPage,
-    });
-    return await modal.present();
   }
 
   async presentToast(msg) {
@@ -70,16 +63,42 @@ export class ProtestsComponent implements OnInit {
     this.dataService.requestCreateProtest({
       title: this.title.value,
       description: this.description.value,
+      startDate: this.startDate.value,
       creatorId: userId,
     });
+
+    if (this.addResult$) {
+      this.presentToast('Success! Form has been submitted.');
+    } else {
+      this.presentToast('Oh no! Something went wrong...');
+    }
   }
 
   get title() {
     return this.form.get('title');
   }
-
   get description() {
     return this.form.get('description');
+  }
+  get startDate() {
+    return this.form.get('startDate');
+  }
+
+  setMinDate() {
+    //format 2021-01-16T19:20+01:00 --> YYYY-MM-DD
+    let date = new Date();
+    let YYYY = date.getFullYear();
+    let MM = '0' + (date.getMonth() + 1).toString().slice(-2);
+    let DD = date.getDate();
+    let currentDateTime = `${YYYY}-${MM}-${DD}`;
+
+    console.log('setMinDate has been called:', currentDateTime);
+    return currentDateTime;
+
+    /* for YYYY-MM-DDDTHH:MMformat ex: 2021-01-16T19:20+01:00
+    let HH = date.getHours();
+    let Mn = date.getMinutes();
+    let currentDateTime = `${YYYY}-${MM}-${DD}T${HH}:${Mn}`; */
   }
 }
 
