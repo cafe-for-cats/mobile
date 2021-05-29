1df4d1135c824f4cd51ef7239ead681a2be60ec1
dgatto <dgattoiii@gmail.com>
2021-05-29 00:23:50 -0400

    Writing user test cases.

--- api/src/users/users.test.ts
+++ api/src/users/users.test.ts
@@ -1,23 +1,39 @@
 import { assert, expect } from 'chai';
+import { constants } from 'http2';
+import { Document } from 'mongoose';
+import sinon, { SinonSandbox, SinonStubbedInstance } from 'sinon';
 import { spy, stub } from 'sinon';
 import { UsersService } from './users.service';
 import * as statics from './users.statics';
 
 describe('UsersService', function () {
   let usersService: UsersService;
+  let staticsMock: SinonStubbedInstance<typeof statics.findUserById>;
 
   beforeEach(() => {
+    staticsMock = sinon.stub(statics, 'findUserById');
     usersService = new UsersService();
   });
 
-  it('should create', () => {
-    const foo = { title: 'hello', description: 'hello' };
-    stub(statics, 'findUserById').returns(Promise.resolve(null));
-    expect(usersService);
+  afterEach(() => {
+    sinon.restore();
   });
 
-  it('should find by user id', () => {
-    const userId = '123';
-    usersService.getUserById(userId);
+  it('should create', () => {
+    expect(usersService).to.be.ok;
+  });
+
+  it('should return false and throw error if no user is found', async () => {
+    const userId = 'invalid id';
+    const u = await usersService.getUserById(userId);
+
+    expect({ status: false, message: 'User not found.' });
+  });
+
+  it('should return true if a user is found', async () => {
+    const doc = new Document();
+    staticsMock = sinon
+      .stub(statics, 'findUserById')
+      .callsFake(async () => await doc);
   });
 });
