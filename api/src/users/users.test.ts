2950fc22a192e6323d910621ba03dadd090442be
dgatto <dgattoiii@gmail.com>
2021-05-30 22:54:37 -0400

    Wire up tests for User Service.

--- api/src/users/users.test.ts
+++ api/src/users/users.test.ts
@@ -1,17 +1,14 @@
-import { assert, expect } from 'chai';
-import { constants } from 'http2';
-import { Document } from 'mongoose';
-import sinon, { SinonSandbox, SinonStubbedInstance } from 'sinon';
-import { spy, stub } from 'sinon';
+import { expect } from 'chai';
+import sinon, { SinonStubbedInstance } from 'sinon';
+import User from './users.models';
 import { UsersService } from './users.service';
 import * as statics from './users.statics';
 
 describe('UsersService', function () {
   let usersService: UsersService;
-  let staticsMock: SinonStubbedInstance<typeof statics.findUserById>;
+  let findByUserIdMock: SinonStubbedInstance<typeof statics.findUserById>;
 
   beforeEach(() => {
-    staticsMock = sinon.stub(statics, 'findUserById');
     usersService = new UsersService();
   });
 
@@ -23,17 +20,56 @@ describe('UsersService', function () {
     expect(usersService).to.be.ok;
   });
 
-  it('should return false and throw error if no user is found', async () => {
-    const userId = 'invalid id';
-    const u = await usersService.getUserById(userId);
+  describe('getUserById', () => {
+    it('should return error and if no user is found', async () => {
+      findByUserIdMock = sinon
+        .stub(statics, 'findUserById')
+        .callsFake(async () => await null);
 
-    expect({ status: false, message: 'User not found.' });
+      const userId = '6088b864a6c9b94094b43e42';
+      const response = await usersService.getUserById(userId);
+
+      expect(response).to.deep.equal({
+        status: false,
+        message: 'User not found.',
+      });
+    });
+
+    it('should return user if a user is found', async () => {
+      findByUserIdMock = sinon
+        .stub(statics, 'findUserById')
+        .callsFake(async () => await user);
+
+      const _id = '6088b864a6c9b94094b43e42';
+      const user = new User({
+        _id,
+        username: 'test-automation',
+        associatedProtests: [],
+      });
+
+      const response = await usersService.getUserById(_id);
+
+      expect(response).to.deep.equal({
+        status: true,
+        message: 'User successfully found.',
+        payload: { user },
+      });
+    });
   });
 
-  it('should return true if a user is found', async () => {
-    const doc = new Document();
-    staticsMock = sinon
-      .stub(statics, 'findUserById')
-      .callsFake(async () => await doc);
-  });
+  // describe('getByUsername', () => {
+  //   it('should return error if user is not found', () => {});
+
+  //   it('should return user if user is found', () => {});
+  // });
+
+  // describe('authenticateUser', () => {
+  //   it('should return error if secret key not set', () => {});
+
+  //   it('should return error if user is not found', () => {});
+
+  //   it('should return error if passwords do not match', () => {});
+  // });
+
+  // describe('registerUser', () => {});
 });
