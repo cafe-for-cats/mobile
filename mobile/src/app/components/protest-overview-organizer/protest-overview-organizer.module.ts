2b0164b7a96348cafd29a479e64033649ef281eb
dgatto <dgattoiii@gmail.com>
2021-06-11 19:19:30 -0400

    Remove unused components; Move `protests` child components.

--- mobile/src/app/components/protest-overview-organizer/protest-overview-organizer.module.ts
+++
@@ -1,9 +0,0 @@
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ProtestOverviewOrganizerComponent } from './protest-overview-organizer.component';
-
-@NgModule({
-  declarations: [ProtestOverviewOrganizerComponent],
-  imports: [CommonModule],
-})
-export class ProtestOverviewOrganizerModule {}
